d4a096f32be420fee7b8a7f3055d2b3e4d20bdeb
yg <yuangang@aiton.com.cn>
2014-06-04 11:15:47 +0800

    简化了开机时间记录函数

--- ComFunc.cpp
+++ ComFunc.cpp
@@ -238,31 +238,10 @@ Return:         нч
 void RecordTscStartTime()
 {
 #ifdef LINUX
-/*
-	FILE* file = NULL;
-	char tmp[64] = {0};
-	struct tm *now;
-	time_t ti;
-
-	file = fopen("TscRun.log","a+");
-	if ( NULL == file )
-	{
-		return;
-	}
-	ti = time(NULL);
-	now = localtime(&ti);
-	sprintf(tmp,"%d-%d-%d %d %d:%02d:%02d TscGb restart.\n", now->tm_year + 1900,
-		now->tm_mon + 1, now->tm_mday, now->tm_wday, now->tm_hour,
-		now->tm_min, now->tm_sec);
-	fputs(tmp,file);
-	fclose(file);
-	AdjustFileSize((char*)"TscRun.log",100);
-*/ 
-	//unsigned long mRestart = 0 ;
-	//(CDbInstance::m_cGbtTscDb).GetSystemData("ucDownloadFlag",mRestart);
-	ACE_OS::system("echo newer version:$(date) tsc restart !>> TscRun.log");
-	#endif
+	ACE_OS::system("echo At $(date) tsc restart !>> TscRun.log");
+#endif
 	CManaKernel::CreateInstance()->SndMsgLog(LOG_TYPE_REBOOT,0,0,0,0);	
+	
 }
 
 /**************************************************************
