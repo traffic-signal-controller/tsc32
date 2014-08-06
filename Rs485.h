32d9b9bd6500c3e8b5f137385c49cae086f8930f
renhualu@live.com <renhualu@live.com>
2014-08-06 14:05:11 +0800

    通信式倒计时修改为独立的文件。包括串口的相关操作

--- Rs485.h
+++ Rs485.h
@@ -4,6 +4,13 @@
 #include "ComStruct.h"
 #define RS485_CPP "CRs485.cpp"
 
+typedef int     INT32;
+typedef short   INT16;
+typedef char    INT8;
+typedef unsigned int UNIT32;
+typedef unsigned short UINT16;
+typedef unsigned char UINT8;
+
 /*
 *´®¿Ú1²Ù×÷Àà
 */
@@ -11,18 +18,21 @@ class CRs485
 {
 public:
 	static CRs485* CreateInstance();
-	static int SetOpt(int fd,int nSpeed,int nBits, char nEvent, int nStop);
 	void OpenRs485();
 	bool Send(Byte* pBuffer, int iSize);
 	bool Recvice(Byte* pBuffer , int iSize);
 	void Reopen();
-
+	INT32 OpenComPort(INT32 ComPort, INT32 baudrate, INT32 databit,const char *stopbit, char parity);
+	void CloseComPort();
+	int getPortFd();
+	INT32 ReadComPort(Byte *data, INT32 datalength);
+	INT32 WriteComPort(Byte * data, INT32 datalength);
+	int SetOpt(int fd,int nSpeed,int nBits, char nEvent, int nStop);
 private:
 	CRs485();
 	~CRs485();
 
-	int  m_iRs485Ctrlfd;
-	int  m_iRs485DataFd;
+	INT32  m_iRs485Ctrlfd;
 	int  m_iRs485Led;
 };
 
